7a56a20e2d2f6421dd4898e98c5775bc5b3b367c
Alexander Rodriguez <alexr1712@gmail.com>
2018-05-06 23:34:40 -0400

    guardar estadistica diaria y borrar informacion temporal

--- cronjob.php
+++ cronjob.php
@@ -8,8 +8,21 @@ $statement = $db->prepare('SELECT * FROM canales');
 $statement->execute();
 $rows = $statement->fetchAll(\PDO::FETCH_ASSOC);
 foreach ($rows as $channel) {
+    // Update Total Members in Main Table
     $member_count = $bot->getChatMembersCount($channel['canal_id']);
     $statement = $db->prepare('UPDATE `canales` SET `cantidad_miembros` = ? WHERE `canales`.`canal_id` = ?;');
     $statement->execute([$member_count, $channel['canal_id']]);
-    sleep(2);
+
+    // Set stats for current day total users
+    $statement = $db->prepare("INSERT INTO canales_stats(canal_id, cant_miembros) VALUES(:channel_id, :cant_miembros)");
+    $statement->execute([
+        'channel_id' => $channel['canal_id'],
+        'cant_miembros' => $member_count
+    ]);
+
+    // Delete logs where datetime is older than 31 days
+    $sql = 'DELETE FROM canales_stats WHERE DATEDIFF(NOW(),fecha) > 31';
+    $statement = $db->prepare($sql);
+    $statement->execute();
 }
+
